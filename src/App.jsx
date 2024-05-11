085fbe17509af294eb618742b3751a9123e1c3a2
ibrahimyryrn <ibrahimyeryaran@gmail.com>
2024-05-11 07:39:08 +0300

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -40,17 +40,15 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div className="app-background">
-        <div className="main-container">
-          <AddItem onAddItem={handleAddItem} />
-          <ItemsList
-            items={items}
-            onDelete={handleDelete}
-            onIncrease={handleIncrease}
-            onDecrease={handleDecrease}
-          />
-        </div>
+    <div className="app-background">
+      <div className="main-container">
+        <AddItem onAddItem={handleAddItem} />
+        <ItemsList
+          items={items}
+          onDelete={handleDelete}
+          onIncrease={handleIncrease}
+          onDecrease={handleDecrease}
+        />
       </div>
     </div>
   );
